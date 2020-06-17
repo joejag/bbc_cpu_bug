df6fa984127061c35b1a880ef2f24c688745a020
joe.wright <joe@joejag.com>
2020-06-17 22:57:40 +0100

    freshly downloaded

---
+++ bbc_sport_files/bundle(26).js
@@ -0,0 +1,1 @@
+Morph.versions["bbc-morph-grandstand@5.4.3"]={"morph-require":"5.2.1"},function(r){function o(t){if(n[t])return n[t].exports;var e=n[t]={exports:{},id:t,loaded:!1};return r[t].call(e.exports,e,e.exports,o),e.loaded=!0,e.exports}var n={};return o.m=r,o.c=n,o.p="",o(0)}([function(r,o,n){Morph.modules["bbc-morph-grandstand@5.4.3/latin"]=n(1)},function(r,o){r.exports={}}]);
